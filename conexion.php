d1464e22b77fab2f31f8d34102ac4a143b160ab9
panama95 <88645866+panama95@users.noreply.github.com>
2021-10-22 21:42:36 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,5 @@
+<?php
+
+$conex = mysqli_connect("localhost","estudiante","utp2021","bd_parcial1"); 
+
+?>
---
+++ registrar_bd.php
@@ -0,0 +1,32 @@
+<?php 
+
+include("conexion.php");
+
+if (isset($_POST['register'])) {
+    if (strlen($_POST['nombre']) >= 1 && strlen($_POST['email']) >= 1) {
+	    $nombre = trim($_POST['nombre']);
+		$apellido = trim($_POST['apellido']);
+	    $email = trim($_POST['email']);
+		$password = trim($_POST['password']);
+		$foto = trim($_POST['foto']);
+		$nivel = 2;
+
+	    $consulta = "INSERT INTO usuarios(nombre, apellido, email, password, foto, nivel) VALUES ('$nombre','$apellido','$email','$password','$foto','$nivel')";
+	    $resultado = mysqli_query($conex,$consulta);
+	    if ($resultado) {
+	    	?> 
+	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
+           <?php
+	    } else {
+	    	?> 
+	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
+           <?php
+	    }
+    }   else {
+	    	?> 
+	    	<h3 class="bad">¡Por favor complete los campos!</h3>
+           <?php
+    }
+}
+
+?>
